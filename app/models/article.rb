6f00c76aaca378ea8776a75d8562081fef10cf76
Adriana <am20dipi@alum.siena.edu>
2021-01-26 18:14:35 -0500

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,18 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+    #attr_accessor :title, :content
+
+   # @@all = []
+
+   # def initialize(id=nil)
+        #@title = title
+       # @content = content
+       # @id = id
+       # @@all << self
+   # end
+
+   # def self.all
+      #  @@all
+  # end
 end
